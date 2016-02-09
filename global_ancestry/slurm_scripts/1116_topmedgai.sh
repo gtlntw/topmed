83b5fd0cace2d269f6e691792887932204b0b529
khlin <khlin@umich.edu>
2016-02-09 16:52:22 -0500

    delete unnecessary backuped up files

---
+++ global_ancestry/slurm_scripts/1116_topmedgai.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+set pipefail; /net/snowwhite/home/khlin/bin/vcftools --remove-indels --remove-filtered-all --max-alleles 2 --gzvcf /net/topmed2/working/hmkang/snps/4318/filtered/chr16.filtered.rehdr.gt2.vcf.gz --gzdiff /net/topmed2/working/khlin/HGDP_938/HGDP_938_chr16_filtered_phased.vcf.gz --diff-site --stdout | awk '{if($4 == "B")  print $1 "\t" $2}' > /net/topmed2/working/khlin/common_site/topmed_chr16_HGDP_common_global.txt
---
+++ global_ancestry/slurm_scripts/480_topmedgai.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+set pipefail; cat /net/topmed2/working/khlin/global_ancestry/temp/NWD243371/NWD243371_filtered_chr18.bim | awk '{print $1"\t"$1":"$4"\t"$3"\t"$4"\t"$5"\t"$6}' > /net/topmed2/working/khlin/global_ancestry/temp/NWD243371/NWD243371_filtered_chr18.bim.tmp && mv /net/topmed2/working/khlin/global_ancestry/temp/NWD243371/NWD243371_filtered_chr18.bim.tmp /net/topmed2/working/khlin/global_ancestry/temp/NWD243371/NWD243371_filtered_chr18.bim
