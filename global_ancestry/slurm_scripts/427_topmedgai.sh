03874701156e25389c0ad48b4d62e8bcafea9e7f
khlin <khlin@umich.edu>
2016-02-09 12:47:04 -0500

    compare freeze1 and freeze2 lai results

---
+++ global_ancestry/slurm_scripts/427_topmedgai.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+set pipefail; cat /net/topmed2/working/khlin/global_ancestry/temp/NWD807382/NWD807382_filtered_chr9.bim | awk '{print $1"\t"$1":"$4"\t"$3"\t"$4"\t"$5"\t"$6}' > /net/topmed2/working/khlin/global_ancestry/temp/NWD807382/NWD807382_filtered_chr9.bim.tmp && mv /net/topmed2/working/khlin/global_ancestry/temp/NWD807382/NWD807382_filtered_chr9.bim.tmp /net/topmed2/working/khlin/global_ancestry/temp/NWD807382/NWD807382_filtered_chr9.bim
---
+++ global_ancestry/slurm_scripts/446_topmedgai.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+set pipefail; cat /net/topmed2/working/khlin/global_ancestry/temp/NWD972722/NWD972722_filtered_chr6.bim | awk '{print $1"\t"$1":"$4"\t"$3"\t"$4"\t"$5"\t"$6}' > /net/topmed2/working/khlin/global_ancestry/temp/NWD972722/NWD972722_filtered_chr6.bim.tmp && mv /net/topmed2/working/khlin/global_ancestry/temp/NWD972722/NWD972722_filtered_chr6.bim.tmp /net/topmed2/working/khlin/global_ancestry/temp/NWD972722/NWD972722_filtered_chr6.bim
