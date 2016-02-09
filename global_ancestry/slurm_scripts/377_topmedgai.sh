196746f7bd8e1c7e67bbe13cbab32e0b1b3e729f
khlin <khlin@umich.edu>
2016-02-09 17:16:33 -0500

    clean up wrong tracked files

--- global_ancestry/slurm_scripts/377_topmedgai.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-set pipefail; cat /net/topmed2/working/khlin/global_ancestry/temp/NWD954967/NWD954967_filtered_chr3.bim | awk '{print $1"\t"$1":"$4"\t"$3"\t"$4"\t"$5"\t"$6}' > /net/topmed2/working/khlin/global_ancestry/temp/NWD954967/NWD954967_filtered_chr3.bim.tmp && mv /net/topmed2/working/khlin/global_ancestry/temp/NWD954967/NWD954967_filtered_chr3.bim.tmp /net/topmed2/working/khlin/global_ancestry/temp/NWD954967/NWD954967_filtered_chr3.bim
