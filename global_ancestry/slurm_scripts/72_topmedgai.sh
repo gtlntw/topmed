fbd344b6b924b7874f428b51dd9b1ee67a75f8b4
khlin <khlin@umich.edu>
2016-02-09 16:42:33 -0500

    Revert "compare freeze1 and freeze2 lai results"
    This reverts commit 03874701156e25389c0ad48b4d62e8bcafea9e7f.

--- global_ancestry/slurm_scripts/72_topmedgai.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-set pipefail; /net/snowwhite/home/khlin/bin/vcftools --remove-indels --remove-filtered-all --max-alleles 2 --gzvcf /net/topmed2/working/hmkang/snps/4318/filtered/chr6.filtered.rehdr.gt2.vcf.gz --gzdiff /net/topmed2/working/khlin/HGDP_938/HGDP_938_chr6_filtered_phased.vcf.gz --diff-site --stdout | awk '{if($4 == "B")  print $1 "\t" $2}' > /net/topmed2/working/khlin/common_site/topmed_chr6_HGDP_common_global.txt
--- global_ancestry/slurm_scripts/897_topmedgai.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-set pipefail; cat /net/topmed2/working/khlin/global_ancestry/temp/NWD260830/NWD260830_filtered_chr17.bim | awk '{print $1"\t"$1":"$4"\t"$3"\t"$4"\t"$5"\t"$6}' > /net/topmed2/working/khlin/global_ancestry/temp/NWD260830/NWD260830_filtered_chr17.bim.tmp && mv /net/topmed2/working/khlin/global_ancestry/temp/NWD260830/NWD260830_filtered_chr17.bim.tmp /net/topmed2/working/khlin/global_ancestry/temp/NWD260830/NWD260830_filtered_chr17.bim
